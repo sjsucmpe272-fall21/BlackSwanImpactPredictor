7ef701050cc3b0004b3e827b0b6f13fe6517f5b3
Sandesh Gupta <sandesh.gupta93@gmail.com>
2021-12-01 11:32:49 -0800

    Caching API data

--- blackswan/src/routes/__init__.py
+++ blackswan/src/routes/__init__.py
@@ -4,12 +4,18 @@ from src.routes.ml_model_2 import process_URL
 
 app_routes = Blueprint("app_routes", "api")
 
+cached_data = {}
+
 @app_routes.post("/process-model")
 def process_model():
   requestJson = request.get_json()
   reqDataType = requestJson['data_type']
 
-  file_content, impact_score, sentiment = process_URL(reqDataType)
+  if(reqDataType in cached_data):
+    file_content, impact_score, sentiment = cached_data[reqDataType]
+  else:
+    file_content, impact_score, sentiment = process_URL(reqDataType)
+    cached_data[reqDataType] = file_content, impact_score, sentiment
 
   return {
     'file_content': file_content,
